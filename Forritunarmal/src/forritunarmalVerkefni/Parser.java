bdad9c0da6d6836fb45858d1f7cbc471bfabfc05
birgir.svavarsson <birgir.svavarsson@GRBPC3597.MAREL.NET>
2016-02-11 22:52:20 +0000

    Parser tilbúinn

--- Forritunarmal/src/forritunarmalVerkefni/Parser.java
+++ Forritunarmal/src/forritunarmalVerkefni/Parser.java
@@ -1,5 +1,6 @@
 package forritunarmalVerkefni;
 
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class Parser {
@@ -16,7 +17,10 @@ public class Parser {
 		myLexer.readInput();
 		myLexer.nextToken();
 		s();
+		
+		print();
 	}
+
 
 	private void s() {
 		if(myLexer.token.getTokenCode().equals(Token.TokenCode.SEMICOL) ){
@@ -27,13 +31,200 @@ public class Parser {
 		}
 		
 		else{
-			
+			if(myLexer.token.getTokenCode().equals(Token.TokenCode.END)){
+				myStack.push(myLexer.token.getLexeme());
+			}	
+			else{
+				myLexer.token.setLexeme("ERRROR");
+				myStack.push(myLexer.token.getLexeme());
+			}
 		}
 	}
 	
+	public void s2(){
+		
+		if(myLexer.token.getLexeme().equals(Token.TokenCode.ID)){
+			myStack.push(myLexer.token.getLexeme());
+			
+			myLexer.nextToken();
+			
+			if(myLexer.token.getLexeme().equals(Token.TokenCode.ASSIGN)){
+				myStack.push(myLexer.token.getLexeme());
+				
+				myLexer.nextToken();
+				expr();
+			}
+			else{
+				myLexer.token.setLexeme("ERRROR");
+				myStack.push(myLexer.token.getLexeme());
+			}	
+		}
+		else if(myLexer.token.getLexeme().equals(Token.TokenCode.PRINT)){
+			myStack.push(myLexer.token.getLexeme());
+			
+			myLexer.nextToken();
+			
+			if(myLexer.token.getLexeme().equals(Token.TokenCode.ID)){
+				myStack.push(myLexer.token.getLexeme());
+				
+				myLexer.nextToken();
+				expr();
+			}
+			else{
+				myLexer.token.setLexeme("ERRROR");
+				myStack.push(myLexer.token.getLexeme());
+			}	
+			
+			
+		}
+	}
 
+	private void expr() {
+		
+		Term();
+		if(myLexer.token.getLexeme().equals(Token.TokenCode.ADD)){
+			myStack.push(myLexer.token.getLexeme());
+			
+			myLexer.nextToken();
+			expr();
+		}
+		else if(myLexer.token.getLexeme().equals(Token.TokenCode.SUB)){
+			myStack.push(myLexer.token.getLexeme());
+			
+			myLexer.nextToken();
+			expr();
+		}
+		
+		
+	}
 
+	private void Term() {
+		Factor();
+		
+		if(myLexer.token.getLexeme().equals(Token.TokenCode.MULT)){
+			myStack.push(myLexer.token.getLexeme());
+			
+			myLexer.nextToken();
+			Term();
+		}
+		
+	}
+
+	private void Factor() {
+		
+		if(myLexer.token.getLexeme().equals(Token.TokenCode.INT)){
+			myStack.push(myLexer.token.getLexeme());
+			
+			myLexer.nextToken();
+		}
+		else if(myLexer.token.getLexeme().equals(Token.TokenCode.ID)){
+			myStack.push(myLexer.token.getLexeme());
+			
+			
+			myLexer.nextToken();
+		}
+		else if(myLexer.token.getLexeme().equals(Token.TokenCode.LPAREN)){
+			myStack.push(myLexer.token.getLexeme());
+			
+			myLexer.nextToken();
+			expr();
+			
+			if(myLexer.token.getLexeme().equals(Token.TokenCode.RPAREN)){
+				myStack.push(myLexer.token.getLexeme());
+				
+				myLexer.nextToken();
+			}
+			else{
+				myLexer.token.setLexeme("ERRROR");
+				myStack.push(myLexer.token.getLexeme());
+				
+				myLexer.nextToken();
+			}	
+			
+		}
+		
+	}
 	
+	private void print() {
+		
+		Stack <String> newStack = new Stack<String>();
+		StringBuilder sb = new StringBuilder();
+		ArrayList<String> strArr = new ArrayList<String>();
+		
+		for(String obj : myStack){
+		
+			if(obj.matches(";")){
+				sb.append(obj);
+				strArr.add(sb.toString());
+				for(int i=0; i<sb.length();i++){
+					sb.delete(i, sb.length());
+				}
+			}
+			if(! obj.matches(";")){
+				sb.append(obj);
+				sb.append(" ");
+			}
+		}
+		
+		for(String obj : strArr){
+			String[] line = obj.split(" ");
+			
+			for(String item : line){
+				
+				if((item.matches("[A-Za-z]+") || item.matches("[0-9]+")) && (! item.matches("print"))){
+					
+						if(item.matches("[0-9]+")){
+							newStack.push("PUSH " + item);
+						}
+						else{
+							newStack.push("PUSH " + item); 
+						}
+					}
+				}
+			
+			reverse(line);
+			for(String item : line){
+				if(item.equals("+")){
+					newStack.push("ADD");
+				}
+				if(item.equals("-")){
+					newStack.push("SUB");
+				}
+				if(item.equals("*")){
+					newStack.push("MULT");
+				}
+				if(item.equals("=")){
+					newStack.push("ASSIGN");
+				}
+				if(item.equals("print")){
+					newStack.push("PRINT");
+				}
+				if(item.equals("ERROR")){
+					newStack.push("Syntax error");
+				}
+			}
+		}
 
+		for(String obj : newStack)
+			{ 
+			    if(obj.equals("Syntax error")){
+			    	System.out.println(obj);
+			    	System.exit(1);
+			    }
+			    else{
+			    	System.out.println(obj);
+			    }
+			}
+	}
+	
+	
+		public static void reverse(Object [] a){
+			for(int i = 0; i < a.length / 2; i++){
+	    		Object temp = a[i]; 
+	    		a[i] = a[a.length - i - 1];
+	    		a[a.length - i - 1] = temp;
+			}
+		}
 
-}
+		
+	}
