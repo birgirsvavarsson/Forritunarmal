133c084718d4dbe767ded25131116b60c090990c
ingi.bragason <ingifreyr1@gmail.com>
2016-02-11 23:29:14 +0100

    Byrjun a interpreter

--- Forritunarmal/src/forritunarmalVerkefni/Interpreter.java
+++ Forritunarmal/src/forritunarmalVerkefni/Interpreter.java
@@ -1,5 +1,28 @@
 package forritunarmalVerkefni;
 
 public class Interpreter {
+	
+	public static void main(String[] args)
+	{
+		fetch();
+		decode();
+		execute();
+	}
+	
+	
+	public void fetch()
+	{
+		
+	}
+	
+	public void decode()
+	{
+		
+	}
+	
+	public void execute()
+	{
+		
+	}
 
 }
