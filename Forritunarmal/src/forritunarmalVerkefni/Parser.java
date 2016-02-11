116f578d0a8f92626c2f1ec0edf7201e8b34ae3c
birgir.svavarsson <birgir.svavarsson@GRBPC3597.MAREL.NET>
2016-02-11 20:45:54 +0000

    Parser

--- Forritunarmal/src/forritunarmalVerkefni/Parser.java
+++ Forritunarmal/src/forritunarmalVerkefni/Parser.java
@@ -1,24 +1,39 @@
 package forritunarmalVerkefni;
 
+import java.util.Stack;
+
 public class Parser {
 	
 	private Token myToken;
 	private Lexer myLexer;
-	
+	private Stack<String> myStack = new Stack<String>();
 	//token = new Token();
 	
 	public Parser(Lexer newLexer) {
-		myToken = new Token();
-		myLexer = new Lexer();
+		//myToken = new Token();
+		//myLexer = new Lexer();
 		myLexer = newLexer;
-	}
-
-	public void parser() {
-		
 		myLexer.readInput();
 		myLexer.nextToken();
-		
+		s();
 	}
+
+	private void s() {
+		if(myLexer.token.getTokenCode().equals(Token.TokenCode.SEMICOL) ){
+			myStack.push(myLexer.token.getLexeme());
+			
+			myLexer.nextToken();
+			s();
+		}
+		
+		else{
+			
+		}
+	}
+	
+
+
+	
 
 
 }
