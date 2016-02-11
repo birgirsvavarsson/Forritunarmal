a8b8262e172ae3487043bf81f14d7cd528c9130e
ingi.bragason <ingifreyr1@gmail.com>
2016-02-11 21:54:33 +0100

    Done I think, double check thooo..

--- Forritunarmal/src/forritunarmalVerkefni/Token.java
+++ Forritunarmal/src/forritunarmalVerkefni/Token.java
@@ -5,7 +5,7 @@ public class Token {
 		String lexeme;
 		TokenCode tCode;
 		
-		enum TokenCode { ID, ASSIGN, SEMICOL, INT, ADD, PLUS, MULT, LPAREN, RPAREN, PRINT, END, ERROR}
+		enum TokenCode { ID, ASSIGN, SEMICOL, INT, ADD, SUB, MULT, LPAREN, RPAREN, PRINT, END, ERROR}
 		
 		public Token() {
 			
